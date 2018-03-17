fb1bbd959be4114175b03b768066cf91290b422b
DmitriyOsipov <osipov.dmal@gmail.com>
2018-03-17 17:23:50 +0200

    ServiceCaller class removed from UI

--- task01/layer01_ui/src/main/java/com/ui/MainUi.java
+++ task01/layer01_ui/src/main/java/com/ui/MainUi.java
@@ -1,5 +1,7 @@
 package com.ui;
 
+import com.services.DateService;
+
 import org.apache.log4j.Logger;
 
 import java.util.Arrays;
@@ -7,15 +9,15 @@ import java.util.Arrays;
 public class MainUi {
 
 
-  private final static Logger logger = Logger.getLogger(ServiceCaller.class);
+  private final static Logger logger = Logger.getLogger(MainUi.class);
 
   public static void main(String[] args) {
 
     logger.info("We are in the layer01_ui module. Input data:" + Arrays.toString(args));
 
     try {
-      ServiceCaller serviceCaller = new ServiceCaller();
-      System.out.println("This day is: " + serviceCaller.callServices(args));
+      DateService dateService = new DateService();
+      System.out.println("This day is: " + dateService.process(args));
     } catch (IllegalArgumentException exc) {
       System.out.println("Date is invalid or format unrecognized");
     }
